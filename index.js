e61f5ac3ad99f6d09980d01d7a8e08cccc7149aa
pramodreddypandiri <98374193+pramodreddypandiri@users.noreply.github.com>
2023-11-07 17:09:03 +0000

    Done

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var cors = require('cors');
+const multer = require('multer'); // For handling file uploads
 require('dotenv').config()
 
 var app = express();
@@ -10,7 +11,25 @@ app.use('/public', express.static(process.cwd() + '/public'));
 app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
+// Configure multer for handling file uploads
+const storage = multer.memoryStorage(); // Store file in memory
+const upload = multer({ storage: storage });
 
+app.post('/api/fileanalyse', upload.single('upfile'), (req, res) => {
+  if (!req.file) {
+    return res.status(400).json({ error: 'No file uploaded' });
+  }
+
+  const file = req.file;
+  const fileSize = file.size;
+  const filename = file.originalname;
+
+  res.json({
+    name: filename,
+    type: file.mimetype,
+    size: fileSize,
+  });
+});
 
 
 
